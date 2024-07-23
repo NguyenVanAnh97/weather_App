7e08b48a9b2a1e44c2b0e080012960c6720d6db8
vace <nguyenvananh270897@gmail.com>
2024-07-23 12:43:42 +0700

    add project

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="text-center mb-6">
+    <h1>{{  new Date().toLocaleDateString('en-us', {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric'
+    }) }}</h1>
+  </div>
+
+  <div>
+    <SeachInput />
+  </div>
+</template>
+
+<script setup>
+  import SeachInput from './components/SeachInput.vue';
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/SeachInput.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <form>
+      <div class="bg-white border border-indigo-600/30 rounded-lg drop-shadow-lg flex items-center">
+        <i class="fa-solid fa-magnifying-glass p-2 text-indigo-600"></i>
+        <input
+          type="text"
+          placeholder="Search for a place"
+          class="rounded-r-lg p-2 border-0 outline-0 focus:ring-2 focus:ring-indigo-600 ring-inset p-2 w-full"
+          v-model="searchTerm.query"
+          @input="handleSearch"
+        />
+      </div>
+    </form>
+
+    <div class="bg-white my-2 rounded-lg shadow-lg">
+      <div v-if="searchTerm.results !== null">
+        <div v-for="place in searchTerm.results" :key="place.name">
+          <button
+            class="px-3 my-2 hover:text-indigo-600 hover:font-bold w-full text-left"
+            @click="getWeather(place.id)"
+          >
+            {{ place.name }} , {{ place.region }} , {{ place.country }}
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { reactive } from 'vue'
+
+const searchTerm = reactive({
+  query: '',
+  timeout: null,
+  results: null
+})
+
+const handleSearch = () => {
+  clearTimeout(searchTerm.timeout) // Giảm Số Lần Gọi API Không Cần Thiết, giảm áp lực lên hệ thống backend
+  searchTerm.timeout = setTimeout(async () => {
+    if (searchTerm.query !== '') {
+      const response = await fetch(
+        `http://api.weatherapi.com/v1/search.json?key=834afe3d28d04f2d9ea165725242207&q=${searchTerm.query}`
+      )
+
+      const data = await response.json()
+      searchTerm.results = data
+      console.log(searchTerm.results)
+    } else {
+      searchTerm.results = null
+    }
+  }, 500)
+}
+
+const getWeather = async (id) => {
+  const response = await fetch(
+    `http://api.weatherapi.com/v1/forecast.json?key=834afe3d28d04f2d9ea165725242207&q=id:${id}&days=3&aqi=no&alerts=no`
+  )
+
+  const data = await response.json()
+  console.log(data)
+}
+</script>
+
+<style scoped></style>
